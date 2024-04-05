2ffda60b987fc3fc24e623715c431e5719378131
Adrizen89 <adrienberard@hotmail.fr>
2024-04-05 11:56:40 +0200

    create custom textfield

--- seeker_app/lib/main.dart
+++ seeker_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seeker_app/widgets/custom_textfield.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Seeker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: CustomTextField(
+            hintText: 'Email',
+          ),
+        ),
+      ),
     );
   }
 }
