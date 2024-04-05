6eed07ee3019d5aea843aca72a6fac8e18b892f9
Adrizen89 <adrienberard@hotmail.fr>
2024-04-05 10:53:05 +0200

    create Colors Palette

---
+++ seeker_app/lib/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColorSelect {
+  static Color mainColor = Color(0xff475956);
+  static Color secondaryColor = Color(0xffF2BB13);
+  static Color accentColor = Color(0xffBF861B);
+  static Color darkColor = Color(0xffA69D8D);
+  static Color lastColor = Color(0xffA6600A);
+
+  static Color grey100 = Color(0xffF7F7F7);
+  static Color grey200 = Color(0xffEBEBEB);
+  static Color grey300 = Color(0xffD1D1D1);
+  static Color grey400 = Color(0xffA6A6A6);
+  static Color grey500 = Color(0xff6B6B6B);
+
+  static Color mainButtonColor = Color(0xff475956);
+  static Color secondaryButtonColor = Color(0xffF2BB13);
+
+  static Color focusFieldColor = Color(0xffD1D1D1);
+  static Color errorFieldColor = Color(0xff);
+  static Color normalFieldColor = Color(0xffEBEBEB);
+}
