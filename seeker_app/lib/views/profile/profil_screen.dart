594129f33b7bfed34a2bb0a4c63a03d3d4cd9cf4
Adrizen89 <adrienberard@hotmail.fr>
2024-04-15 15:55:23 +0200

    Head of profil screen

--- seeker_app/lib/views/profile/profil_screen.dart
+++ seeker_app/lib/views/profile/profil_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:seeker_app/constants/colors.dart';
+import 'package:seeker_app/constants/size.dart';
 import 'package:seeker_app/providers/user_data_provider.dart';
 
 class ProfilScreen extends StatefulWidget {
@@ -15,34 +18,62 @@ class _ProfilScreenState extends State<ProfilScreen> {
     final userProfile = Provider.of<UserProvider>(context).userProfile;
 
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (userProfile.photoUrl != null)
-            CircleAvatar(
-              backgroundImage: NetworkImage(userProfile.photoUrl!),
-              radius: 60.0,
-            )
-          else
-            CircleAvatar(
-              child: Icon(Icons.person, size: 60),
-              radius: 60.0,
+      body: Stack(
+        children: <Widget>[
+          // Création des deux demies du fond
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: SizeConfig.screenHeight * 0.4,
+            child: Container(
+              padding: EdgeInsets.only(
+                  right: SizeConfig.customPadding(),
+                  left: SizeConfig.customPadding()),
+              color: ColorSelect.mainColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        radius: 70,
+                      ),
+                      CircleAvatar()
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("${userProfile.firstName} ${userProfile.lastName}"),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("${userProfile.biography}"),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
             ),
-          Container(
-            child: Text('Bonjour, ${userProfile.firstName}'),
           ),
-          Container(
-            child: Text("Née le ${userProfile.dateNaissance}"),
+          Center(
+            child: Row(
+              children: [
+                Column(
+                  children: [Text("67"), Text("découvertes")],
+                ),
+                Column(
+                  children: [Text("inscrit le"), Text("27/08/2023")],
+                )
+              ],
+            ),
           ),
-          Container(
-            child: Text(userProfile.biography),
-          ),
-          Container(
-            child: Text("65 découvertes"),
-          )
         ],
       ),
-    ));
+    );
   }
 }
