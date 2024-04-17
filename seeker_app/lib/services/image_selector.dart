8aaaf1a61731c85b0cc7202597c12e3f49b38e1a
Adrizen89 <adrienberard@hotmail.fr>
2024-04-17 15:58:13 +0200

    Add function for add img and other img

--- seeker_app/lib/services/image_selector.dart
+++ seeker_app/lib/services/image_selector.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ImageSelector {
   final ImagePicker _picker = ImagePicker();
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   Future<File?> pickImage() async {
     final XFile? pickedFile =
@@ -12,5 +14,20 @@ class ImageSelector {
       return File(pickedFile.path);
     }
     return null;
+  }
+
+  Future<String?> uploadImage(File imageFile, String userId) async {
+    try {
+      String filePath =
+          'markers/$userId/${DateTime.now().millisecondsSinceEpoch}.jpg';
+      Reference ref = _storage.ref().child(filePath);
+      UploadTask uploadTask = ref.putFile(imageFile);
+      TaskSnapshot snapshot = await uploadTask;
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
