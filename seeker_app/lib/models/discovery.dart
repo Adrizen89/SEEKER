911261bd1d7b7df826f9b9337215ca84c8586b1c
Adrizen89 <adrienberard@hotmail.fr>
2024-04-18 11:26:58 +0200

    Modify design Card

--- seeker_app/lib/models/discovery.dart
+++ seeker_app/lib/models/discovery.dart
@@ -7,13 +7,28 @@ class Discovery {
   final DateTime? date;
   final double? lat;
   final double? lng;
+  final List<String>? imgUrls;
 
-  Discovery(
-      {this.id, this.title, this.description, this.date, this.lat, this.lng});
+  Discovery({
+    this.id,
+    this.title,
+    this.description,
+    this.date,
+    this.lat,
+    this.lng,
+    this.imgUrls,
+  });
 
   factory Discovery.fromFirestore(DocumentSnapshot doc) {
     var data = doc.data() as Map<String, dynamic>?; // Utilisez un cast sûr
     if (data != null) {
+      // Assurez-vous que 'imgUrls' est traité comme une liste de chaînes de caractères
+      var imgUrlsDynamic = data['imgUrls'];
+      List<String>? imgUrlsList;
+      if (imgUrlsDynamic is List) {
+        imgUrlsList = imgUrlsDynamic.map((item) => item.toString()).toList();
+      }
+
       return Discovery(
         id: doc.id,
         title: data['title'] as String?, // Assurez-vous que le cast est sûr
@@ -22,12 +37,21 @@ class Discovery {
         date: data['date'] != null
             ? DateTime.fromMillisecondsSinceEpoch(data['date'] as int)
             : null,
-        lat: data['latitude'] as double,
-        lng: data['longitude'] as double,
+        lat: data['latitude'] as double?,
+        lng: data['longitude'] as double?,
+        imgUrls: imgUrlsList,
       );
     } else {
       // Gérez le cas où data est null
-      return Discovery(); // Vous pouvez choisir de retourner un objet vide ou lever une exception
+      return Discovery(
+        id: '',
+        title: '',
+        description: '',
+        date: DateTime.now(),
+        lat: 0.0,
+        lng: 0.0,
+        imgUrls: [],
+      ); // Retournez un objet vide avec des valeurs par défaut
     }
   }
 }
